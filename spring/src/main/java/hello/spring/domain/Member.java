5a009fc33e3490162198fbfad16c8f2f29ecd479
euiyang <euiyang2000@gmail.com>
2022-06-07 16:18:47 +0900

    JdbcTemplate,JPA

--- spring/src/main/java/hello/spring/domain/Member.java
+++ spring/src/main/java/hello/spring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.spring.domain;
 
+import javax.persistence.*;
+
+@Entity//JPA가 관리하는 entity가 된다.
 public class Member {
+
+    @Id//pk다
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//자동 생성=IDENTITY
     private Long id;
+
+    @Column(name="usertname")
     private String name;
 
     public Long getId() {
--- spring/src/main/java/hello/spring/repository/MemoryMemberRepository.java
+++ spring/src/main/java/hello/spring/repository/MemoryMemberRepository.java
@@ -4,13 +4,13 @@ import hello.spring.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
-@Repository
+//@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store= new HashMap<>();
     private static long sequence=0L;
 
-    @Override
+//    @Override
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(),member);

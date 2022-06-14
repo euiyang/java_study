1813ccc01f8cb5fdfdfb70a5262f66226a044d2d
euiyang <euiyang2000@gmail.com>
2022-06-14 15:00:06 +0900

    예제2까지

---
+++ spring_begin/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }// 마치 외부(AppConfig)에서 의존 관계를 넣어주는 것으로 보임= 의존관계 주입
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ spring_begin/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+//    private final MemberRepository memberRepository=new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+    //고치면 클라이언트를 고치게 된다-> OCP 위반
+    //discountPolicy 뿐만 아니라 FixDiscountPolicy도 의존
+    //인터페이스, 구체화 전부 의존-> DIP 위반
+
+    private final MemberRepository memberRepository;//final이면 생성자로 할당 되어야함.
+    private final DiscountPolicy discountPolicy;
+    //->인터페이스에만 의존. 하지만 다른 방법으로 객체를 생성하고 주입해야 한다.
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member=memberRepository.findById(memberId);
+        int discountPrice= discountPolicy.discount(member,itemPrice);
+        return new Order(memberId,itemName,itemPrice,discountPrice);
+    }
+}

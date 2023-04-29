91f08c1e2dbf86d9e44ad100d774285e704656f5
euiyang <euiyang2000@gmail.com>
2023-04-29 20:53:38 +0900

    setting

---
+++ ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/Ex1HelloJpaApplication.java
@@ -0,0 +1,17 @@
+package jpabasic.ex1hellojpa;
+
+import javax.persistence.*;
+
+
+public class Ex1HelloJpaApplication {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+
+	}
+
+}
